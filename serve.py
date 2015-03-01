0cf6d5d02126fa01df02ffb173f107cd55fef032
theepicsnail <theepicsnail@gmail.com>
2015-02-28 22:22:00 -0800

    Add an error message if the bot is started without a config

--- serve.py
+++ serve.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python2
 
+try:
+    from src.config.config import *
+except:
+    print "You forgot to create a config! Try working from the example:"
+    print "Do: mv src/config/config_example.py src/config/config.py"
+    print "then edit src/config/config.py"
+    exit(1)
+
 from sys import argv
 from src.bot import *
-from src.config.config import *
 import datetime
 
 #Logger is run. Roboraj is contained within
